9b22f01b3802bc5008f6506562ad54ad6501af12
Ryan Hodges <ryan.d.hodges@gmail.com>
2022-06-01 12:01:02 -0700

    updated dev deployment

--- bluepages/bluepages/settings.py
+++ bluepages/bluepages/settings.py
@@ -20,12 +20,14 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-jj4q*@3$n&d6kxl$y)oinau1rz2)+1(#5)9_k@*+*p4dj%id)0'
+SECRET_KEY = 'FIX_THIS_FOR_PRODUCTION'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    '*',
+]
 
 
 # Application definition
@@ -75,8 +77,12 @@ WSGI_APPLICATION = 'bluepages.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.contrib.gis.db.backends.postgis',
+        'NAME': 'bluepages',
+        'USER': 'postgres',
+        # 'PASSWORD': '',
+        # 'HOST': 'localhost',
+        # 'PORT': 5432,
     }
 }
 
--- deploy/linux_pyenv.sh
+++ deploy/linux_pyenv.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-cd /usr/local/apps/wcbluepages/
+cd /usr/local/apps/
 python3 -m pip install --user virtualenv
 virtualenv env --python=python3
-source /usr/local/apps/wcbluepages/env/bin/activate
+source /usr/local/apps/env/bin/activate
 
 pip install -r /usr/local/apps/wcbluepages/requirements.txt
