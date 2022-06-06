e8fdbaf835b80632cdf543417e92f46063731fae
Ryan Hodges <ryan.d.hodges@gmail.com>
2022-06-06 09:43:14 -0700

    production stack installation script

---
+++ deploy/production_linux_deps.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+apt update
+apt upgrade -y
+apt install python3 python3-pip python3-virtualenv virtualenv gcc make uwsgi uwsgi-plugin-python3 gdal-bin python3-gdal python3-dev build-essential nginx libproj-dev proj-bin -y
+
+# 22.04 Jammy
+apt install postgresql-14 postgresql-contrib postgresql-server-dev-14 postgis postgresql-14-postgis-3 -y
