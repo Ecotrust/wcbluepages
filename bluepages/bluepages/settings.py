84a190178b97f93c8fce80128e49b2bf5b0da80f
Ryan Hodges <ryan.d.hodges@gmail.com>
2022-06-08 09:35:52 -0700

    lock that debug down

--- bluepages/bluepages/settings.py
+++ bluepages/bluepages/settings.py
@@ -22,8 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'FIX_THIS_FOR_PRODUCTION'
 
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '*',
