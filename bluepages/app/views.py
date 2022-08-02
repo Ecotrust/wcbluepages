4f227a59729bca694da4bd3dd36ec96ee1a45ca8
Ryan Hodges <ryan.d.hodges@gmail.com>
2022-08-02 10:32:39 -0700

    new view to render regions to geojson response

--- bluepages/app/views.py
+++ bluepages/app/views.py
@@ -1,8 +1,36 @@
+from django.http import JsonResponse
 from django.shortcuts import render
-
+from app.models import Region
+import json
 # Create your views here.
 def home(request):
 
     context = {}
 
     return render(request, "welcome.html", context)
+
+
+
+
+####################################
+#   REGION PICKER                  #
+####################################
+
+def regionJSON(request):
+    regions = Region.objects.all();
+    geojson = {
+        "type": "FeatureCollection",
+        "features": []
+    }
+
+    for region in regions:
+        region_json = json.loads(region.geometry.json)
+        region_json['properties'] = {
+            'id': region.id,
+            'name': region.name,
+            'depth': region.depth_type
+        };
+        regions['features'].append(region_json)
+    
+    return JsonResponse(regions)
+
