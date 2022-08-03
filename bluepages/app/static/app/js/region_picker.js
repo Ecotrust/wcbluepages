c9f1e34c0eaf4dd3deb41c5f35b8c60736ddcc1e
Ryan Hodges <ryan.d.hodges@gmail.com>
2022-08-03 09:14:06 -0700

    static region json, region picker functionally complete

--- bluepages/app/static/app/js/region_picker.js
+++ bluepages/app/static/app/js/region_picker.js
@@ -26,6 +26,9 @@ const styleFunction = function(feature) {
 
 const VectorSource = new ol.source.Vector({
     features: []
+    // url: '/regions.json',
+    // url: '/static/app/data/regions.json',
+    // format: new ol.format.GeoJSON()
 });
 
 const vectorLayer = new ol.layer.Vector({
@@ -50,11 +53,75 @@ const map = new ol.Map({
     })
 });
 
+// Selection logic largely taken from OL examples:
+//  https://openlayers.org/en/latest/examples/select-features.html
+//  https://openlayers.org/en/latest/examples/select-multiple-features.html
+
+let selected = [];
+
+function selectedStyleFunction(feature) {
+    var label = getLabel(feature);
+    let selectedStyle = new ol.style.Style({
+        fill: new ol.style.Fill({
+            color: 'rgba(255,255,255,0.1)'
+        }),
+        stroke: new ol.style.Stroke({
+            color: 'rgba(255,0,255,1.0)',
+            width: 2,
+        }),
+        text: new ol.style.Text({
+            text: label,
+            stroke: new ol.style.Stroke({
+                color: 'white',
+                width: 1
+            }),
+            font: 'bold 12px sans-serif'
+        })
+    })
+    return selectedStyle;
+}
+
+let code_array = [];
+// let name_array = [];
+const updateSelectedRegionCodes = function() {
+    code_array = [];
+    // name_array = [];
+    for (let i = 0; i < selected.length; i++) {
+        code_array.push(selected[i].get('id'));
+        // name_array.push(selected[i].get('name'));
+    }
+    code_array.sort();
+    // name_array.sort();
+    $('#region-codes').val(code_array.join(';'));
+    // $('#region-name').val(name_array.join(';'));
+}
+
+
+map.on('singleclick', function(e) {
+    map.forEachFeatureAtPixel(e.pixel, function (f) {
+        const selIndex = selected.indexOf(f);
+        if (selIndex < 0) {
+            selected.push(f);
+            f.setStyle(selectedStyleFunction(f));
+        } else {
+            selected.splice(selIndex, 1);
+            f.setStyle(undefined);
+        }
+    });
+
+    // manage 'selected codes' box now.
+    updateSelectedRegionCodes();
+
+});
+
 $.ajax({
-    url:'/regions.json',
+    // url:'/regions.json',
+    url:'/static/app/data/regions.json',
     dataType: 'json'
 })
-    .done(function(data) {
-        var features = new ol.format.GeoJSON().readFeatures(data);
-        VectorSource.addFeatures(features);
-    });
+.done(function(data) {
+    var features = new ol.format.GeoJSON().readFeatures(data);
+    VectorSource.addFeatures(features);
+
+    
+});
