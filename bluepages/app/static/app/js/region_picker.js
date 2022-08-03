ddbaf121abb40956f08e500e429887af125d95d1
Ryan Hodges <ryan.d.hodges@gmail.com>
2022-08-02 17:37:19 -0700

    adding regions to picker map via json API, leveraging sass-processor

--- bluepages/app/static/app/js/region_picker.js
+++ bluepages/app/static/app/js/region_picker.js
@@ -1,14 +1,33 @@
+const VectorSource = new ol.source.Vector({
+    features: []
+});
 
+const vectorLayer = new ol.layer.Vector({
+    source: VectorSource,
+});
 
 const map = new ol.Map({
     layers: [
         new ol.layer.Tile({
             source: new ol.source.OSM
-        })
+        }),
+        vectorLayer
     ],
     target: 'map',
     view: new ol.View({
-        center: [-120,45],
+        center: [
+            -13803616.858365921,    // -124
+            4865942.279503175       // 40 
+        ],
         zoom: 5,
     })
 });
+
+$.ajax({
+    url:'/regions.json',
+    dataType: 'json'
+})
+    .done(function(data) {
+        var features = new ol.format.GeoJSON().readFeatures(data);
+        VectorSource.addFeatures(features);
+    });
