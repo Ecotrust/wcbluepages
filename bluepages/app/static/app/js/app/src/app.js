d4f663ec9035ec7e7d1255c10705680bda957e1c
Ryan Hodges <ryan.d.hodges@gmail.com>
2022-12-16 15:35:33 -0800

    launch contact form via button into modal and save

--- bluepages/app/static/app/js/app/src/app.js
+++ bluepages/app/static/app/js/app/src/app.js
@@ -1,6 +1,28 @@
-import 'bootstrap';
+import { Modal } from 'bootstrap';
 import * as $ from 'jquery';
 import DataTable from 'datatables';
+
+app.loadSuggestionForm = function() {
+    $.ajax({
+        url: "/suggestion_form",
+        success: function(form){
+            $("#suggestionModalWrapper").html(form);
+            app.suggestionModal.show();
+        }
+    })
+}
+
+app.submitContactSuggestion = function() {
+    let contact_form = $("#contact-suggestion-form");
+    let submitAction = contact_form .attr('action');
+
+    // TODO: Validate form
+
+    $.post(submitAction, contact_form.serialize(), function(data){window.alert('Contact submitted!')});
+}
+
+app.suggestionModal = new Modal(document.getElementById('suggestionModal'), {});
+
 
 $(document).ready( function () {
     $('#contact-results-table').DataTable();
