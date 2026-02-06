45f239195a0f8f2b00f3c09e870cc6d361bb7f70
Paige Williams <pwilliams@ecotrust.org>
2026-02-06 11:33:23 -0800

    update python to 3.11

--- deploy/linux_deps.sh
+++ deploy/linux_deps.sh
@@ -2,7 +2,7 @@
 
 apt update
 apt upgrade -y
-apt install python3 python3-pip python3-virtualenv virtualenv gcc make uwsgi uwsgi-plugin-python3 gdal-bin python3-gdal python3-dev build-essential nginx libproj-dev proj-bin -y
+apt install python3.11 python3.11-dev python3.11-venv python3-pip python3-virtualenv virtualenv gcc make uwsgi uwsgi-plugin-python3 gdal-bin python3-gdal build-essential nginx libproj-dev proj-bin -y
 
 # 20.04 Focal
 # apt install postgresql-12 postgresql-contrib postgresql-server-dev-12 postgis postgresql-12-postgis-3 -y
--- deploy/production_linux_deps.sh
+++ deploy/production_linux_deps.sh
@@ -2,7 +2,7 @@
 
 apt update
 apt upgrade -y
-apt install python3 python3-pip python3-virtualenv virtualenv gcc make uwsgi uwsgi-plugin-python3 gdal-bin python3-gdal python3-dev build-essential nginx libproj-dev proj-bin munin munin-node -y
+apt install python3.11 python3.11-dev python3.11-venv python3-pip python3-virtualenv virtualenv gcc make uwsgi uwsgi-plugin-python3 gdal-bin python3-gdal build-essential nginx libproj-dev proj-bin munin munin-node -y
 
 # 22.04 Jammy
 apt install postgresql-14 postgresql-contrib postgresql-server-dev-14 postgis postgresql-14-postgis-3 -y
