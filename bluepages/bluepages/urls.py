ad4a1ddad96ef431e02da253af520846e97b0410
Paige Williams <pwilliams@ecotrust.org>
2026-02-11 14:53:01 -0800

    reset migrations and update fixtures

--- bluepages/bluepages/urls.py
+++ bluepages/bluepages/urls.py
@@ -14,24 +14,27 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.contrib.sitemaps import GenericSitemap
+from django.contrib.sitemaps import Sitemap
 from django.contrib.sitemaps.views import sitemap
 from django.urls import path, re_path, include
 from app import urls as app_urls
 from app.views import adminSuggestionReviewMenu, adminSuggestionRejection
 from app.models import Contact, Region, Record, Entity
 
-contact_dict = {
-    'queryset': Contact.objects.filter(pk__in=[contact.pk for contact in Contact.objects.filter(is_test_data=False) if contact.public]),
-    'date_field': 'date_modified',
-}
+class ContactSitemap(Sitemap):
+    """Lazy sitemap that only queries database when accessed, not at import time."""
+    def items(self):
+        return [contact for contact in Contact.objects.filter(is_test_data=False) if contact.public]
+    
+    def lastmod(self, obj):
+        return obj.date_modified
 
 urlpatterns = [
     path('admin/app/contactsuggestion/<int:suggestion_id>/review-suggestion/', adminSuggestionReviewMenu),
     path('admin/app/contactsuggestion/<int:suggestion_id>/reject/', adminSuggestionRejection),
     path('admin/', admin.site.urls),
 
-    path('sitemap.xml', sitemap, {'sitemaps': {'contact': GenericSitemap(contact_dict)}}, name='django.contrib.sitemaps.views.sitemap'),
+    path('sitemap.xml', sitemap, {'sitemaps': {'contact': ContactSitemap}}, name='django.contrib.sitemaps.views.sitemap'),
 
     re_path(r'', include(app_urls)),
 ]
