5db2d989b22f575ca8e332e73420a7bfb9316652
Paige Williams <pwilliams@ecotrust.org>
2026-05-01 12:28:24 -0700

    begin docker infra for production deployment

--- bluepages/bluepages/settings.py
+++ bluepages/bluepages/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "FIX_THIS_FOR_PRODUCTION"
+SECRET_KEY = os.environ.get("SECRET_KEY", "your-secret-key")
 
-DEBUG = False
+DEBUG = os.environ.get("DEBUG", "False")
 DEBUG_JS = DEBUG
 
 ALLOWED_HOSTS = [
--- entrypoint.sh
+++ entrypoint.sh
@@ -11,8 +11,20 @@ echo "PostgreSQL is ready!"
 echo "Running database migrations..."
 python manage.py migrate --noinput
 
+echo "Compiling SCSS files..."
+python manage.py compilescss
+
 echo "Collecting static files..."
 python manage.py collectstatic --noinput
+chmod -R 755 /app/bluepages/static_root
 
-echo "Starting python development server on :8000"
-python manage.py runserver 0.0.0.0:8000
+if [ "$1" = "prod" ]; then
+    echo "Starting Gunicorn on :8000"
+    gunicorn bluepages.wsgi:application --bind 0.0.0.0:8000
+elif [ "$1" = "dev" ]; then
+    echo "Starting python development server on :8000"
+    python manage.py runserver 0.0.0.0:8000
+else
+    # Default to the passed command if not 'prod' or 'dev'
+    exec "$@"
+fi
