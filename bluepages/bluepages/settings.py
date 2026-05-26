b5f84e01ab9ef0657d3ecb1d40c1c6321b489c39
Paige Williams <pwilliams@ecotrust.org>
2026-05-26 14:23:54 -0700

    continue debugging ses set up

--- bluepages/bluepages/settings.py
+++ bluepages/bluepages/settings.py
@@ -111,11 +111,11 @@ LOGGING["handlers"]["mail_admins"]["include_html"] = True
 
 # EMAIL
 EMAIL_BACKEND = "django.core.mail.backends.smtp.EmailBackend"
-EMAIL_HOST = os.environ.get("EMAIL_HOST", "localhost")  # your SES region
-EMAIL_PORT = 25
-EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "")  # your SES SMTP username
-EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")  # your SES SMTP password
-EMAIL_USE_TLS = False
+EMAIL_HOST = os.environ.get("EMAIL_HOST", "localhost")
+EMAIL_PORT = 587
+EMAIL_HOST_USER = os.environ.get("EMAIL_HOST_USER", "")
+EMAIL_HOST_PASSWORD = os.environ.get("EMAIL_HOST_PASSWORD", "")
+EMAIL_USE_TLS = True
 DEFAULT_FROM_EMAIL = "WC Blue Pages<noreply@bluepages.ecotrust.org>"
 
 # Registration
--- infra/ses.tf
+++ infra/ses.tf
@@ -5,3 +5,8 @@ resource "aws_ses_domain_identity" "bluepages" {
 resource "aws_ses_domain_dkim" "bluepages" {
   domain = aws_ses_domain_identity.bluepages.domain
 }
+
+resource "aws_ses_domain_mail_from" "bluepages" {
+  domain           = aws_ses_domain_identity.bluepages.domain
+  mail_from_domain = "noreply.${aws_ses_domain_identity.bluepages.domain}"
+}
